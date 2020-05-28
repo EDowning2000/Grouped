96edfbf8a6978d2cbf7dfe2ca8e2e1a8b17a99f4
EDowning2000 <emersondowning@gmail.com>
2020-05-27 21:27:37 -0400

    database file setup

---
+++ client/db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS events_db;
+
+CREATE DATABASE events_db;
+
+CREATE TABLE events(
+  id INT NOT NULL auto_increment, 
+  photo VARCHAR (300),
+  event_name VARCHAR (300) NOT NULL,
+  zip_code VARCHAR (100) NOT NULL,
+  year VARCHAR (40) NOT NULL,
+  month VARCHAR (40) NOT NULL,
+  day VARCHAR (40) NOT NULL,
+  description VARCHAR (2000) NULL,
+  event_host VARCHAR (300) NOT NULL
+)

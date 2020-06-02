74fdd56ac4d779394e8863f366b39e3ec877098a
EDowning2000 <emersondowning@gmail.com>
2020-06-01 21:09:52 -0400

    signup and signin are finished

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+ 
+function HomePage(){
+  return(
+    <div className="container">
+      <div className="titlePage">
+        <h1 className="header">Welcome to Linked!</h1>
+          <p className="description">A place to find events going on around you</p>
+
+      </div>
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
---
+++ client/src/components/Wrapper.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Wrapper (){
+  return (
+    <div className="wrapper">
+
+    </div>
+  )
+};
+
+module.exports= Wrapper; 

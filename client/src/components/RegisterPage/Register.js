0d92486de403d6d0f2f1b831e66f9d1faeaed341
EDowning2000 <emersondowning@gmail.com>
2020-06-10 17:19:14 -0400

    register html is in place, just need to import the rectangles

---
+++ client/src/components/RegisterPage/Register.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import './style.css';
+
+
+const firstDivStyle ={
+  margin:"0",
+  background: "rgba(22,24,56,1.0)"
+};
+  const startStyle ={
+    width: "1024px",
+     height: "100%",
+      position:"relative",
+       margin:"auto"
+  };
+
+  function Register(){
+    return (
+<div style={firstDivStyle}>
+        <input id="anPageName" name="page" type="hidden" value="register"/>
+        <div class="register anima-word-break ">
+            <div style={startStyle}>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy-4@2x.png" class="rectangle anima-animate-enter" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy@2x.png" class="rectanglecopy anima-animate-enter1" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy-2@2x.png" class="rectanglecopy2 anima-animate-enter2" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy-4@2x.png" class="rectanglecopy3" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy-4@2x.png" class="rectanglecopy4" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-1@2x.png" class="rectangle1 anima-animate-enter3" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy-8@2x.png" class="rectanglecopy8 anima-animate-enter4" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy-9@2x.png" class="rectanglecopy9 anima-animate-enter5" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy-4@2x.png" class="rectanglecopy5 anima-animate-enter6" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy-6.png" class="rectanglecopy6 anima-animate-enter7" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy-13@2x.png" class="rectanglecopy13 anima-animate-enter8" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="Image" anima-src="./img/register-rectangle-copy-7@2x.png" class="rectanglecopy7 anima-animate-enter9" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <div class="joinus">
+                    Join Us
+                </div>
+                <div class="rectangle2">
+                </div>
+                <div class="rectanglecopy10">
+                </div>
+                <img alt="Image" anima-src="./img/register-rectangle-copy-11.png" class="rectanglecopy11" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+                
+                <div class="username">
+                    username
+                </div>
+                <div class="password">
+                    password
+                </div>
+                <div class="join anima-smart-layers-pointers ">
+                    join
+                </div>
+                <div class="grouped">
+                    Grouped
+                </div>
+                <div class="rectanglecopy101">
+                </div>
+                <div class="confirmpassword">
+                    confirm password
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+  export default Register;

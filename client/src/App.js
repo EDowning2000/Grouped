b1cdd08dc67eb324ba00d31ce6020048102f51e9
EDowning2000 <emersondowning@gmail.com>
2020-06-11 14:05:41 -0400

    routes are inputed into the app component instead of the index component

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,30 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
+import HomePage from "./components/HomePage/HomePage";
+import AboutPage from "./components/AboutPage/AboutPage";
+import CreateEventPage from "./components/CreateEventPage/CreateEventPage";
+import LandingPage from "./components/LandingPage/LandingPage";
+import SearchEventPage from "./components/SearchEventPage/SearchEventPage";
 import UserProfilePage from "./components/UserProfilePage/UserProfilePage";
 import Wrapper from "./components/Wrapper/Wrapper";
-import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <Wrapper>
-        <LandingPage></LandingPage>
+        <Router>
+        <Switch>
+          <Route path="/" component={LandingPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/home" component={HomePage} />
+          <Route path="/user" component={UserProfilePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/create" component={CreateEventPage} />
+          <Route path="/search" component={SearchEventPage} />
+        </Switch>
+        </Router>
       </Wrapper>
     </div>
   );

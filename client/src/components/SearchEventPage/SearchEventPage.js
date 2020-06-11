a23b95d5d6835b5644b6beea85ebb694e5b5f71c
EDowning2000 <emersondowning@gmail.com>
2020-06-10 20:23:26 -0400

    rectangles have been imported for the search event

--- client/src/components/SearchEventPage/SearchEventPage.js
+++ client/src/components/SearchEventPage/SearchEventPage.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import './style.css'
 
+import rectangle1 from '../../SearchEventImg/search-event-rectangle-1@2x.png';
+import rectangle2 from '../../SearchEventImg/search-event-rectangle-2@2x.png';
+import rectangle0 from '../../SearchEventImg/search-event-rectangle-copy-2@2x.png';
+import rectangle5 from '../../SearchEventImg/search-event-rectangle-copy-5@2x.png';
+import rectangle6 from '../../SearchEventImg/search-event-rectangle-copy-6.png';
+import rectangle7 from '../../SearchEventImg/search-event-rectangle-copy-7@2x.png';
+import rectangle8 from '../../SearchEventImg/search-event-rectangle-copy-8@2x.png';
+import rectangle9 from '../../SearchEventImg/search-event-rectangle-copy-9@2x.png';
+import rectangle13 from '../../SearchEventImg/search-event-rectangle-copy-13@2x.png';
+import rectangle14 from '../../SearchEventImg/search-event-rectangle-copy@2x.png';
 
 const firstDivStyle ={
   margin:"0",
@@ -13,27 +23,37 @@ const firstDivStyle ={
        margin:"auto"
   };
 
-
-
-
 function SearchEventPage(){
   return(
     <div style={firstDivStyle}>
         <input id="anPageName" name="page" type="hidden" value="searchevent"/>
         <div className="searchevent anima-word-break ">
             <div style={startStyle}>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy-5@2x.png" className="rectangle anima-animate-enter" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy@2x.png" className="rectanglecopy anima-animate-enter1" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy-2@2x.png" className="rectanglecopy2 anima-animate-enter2" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy-5@2x.png" className="rectanglecopy3" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy-5@2x.png" className="rectanglecopy4" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-1@2x.png" className="rectangle1 anima-animate-enter3" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy-8@2x.png" className="rectanglecopy8 anima-animate-enter4" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy-9@2x.png" className="rectanglecopy9 anima-animate-enter5" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy-5@2x.png" className="rectanglecopy5 anima-animate-enter6" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy-6.png" className="rectanglecopy6 anima-animate-enter7" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy-13@2x.png" className="rectanglecopy13 anima-animate-enter8" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-                <img alt="Image" anima-src="./img/search-event-rectangle-copy-7@2x.png" className="rectanglecopy7 anima-animate-enter9" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy-5@2x.png" className="rectangle anima-animate-enter" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy@2x.png" className="rectanglecopy anima-animate-enter1" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy-2@2x.png" className="rectanglecopy2 anima-animate-enter2" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy-5@2x.png" className="rectanglecopy3" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy-5@2x.png" className="rectanglecopy4" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-1@2x.png" className="rectangle1 anima-animate-enter3" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy-8@2x.png" className="rectanglecopy8 anima-animate-enter4" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy-9@2x.png" className="rectanglecopy9 anima-animate-enter5" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy-5@2x.png" className="rectanglecopy5 anima-animate-enter6" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy-6.png" className="rectanglecopy6 anima-animate-enter7" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy-13@2x.png" className="rectanglecopy13 anima-animate-enter8" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-copy-7@2x.png" className="rectanglecopy7 anima-animate-enter9" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
                 <div className="grouped">
                     Grouped
                 </div>
@@ -46,7 +66,8 @@ function SearchEventPage(){
                 <div className="enterzip">
                     Enter Zip
                 </div>
-                <img alt="Image" anima-src="./img/search-event-rectangle-2@2x.png" className="rectangle2" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+                <img alt="rectangle pic" anima-src="./img/search-event-rectangle-2@2x.png" className="rectangle2" src="data:rectangle pic/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+
                 <div className="search anima-smart-layers-pointers ">
                     Search
                 </div>

bfb1aafcb69eb8897589411c4a1abe67fb6c4ba5
EDowning2000 <emersondowning@gmail.com>
2020-06-12 13:45:44 -0400

    user database file updated with validation for the zip code, and the register file was implimented with full validation and error handling

--- models/user.js
+++ models/user.js
@@ -1,30 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  email:{
+  email: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
-    type: String, 
-    required: true
-  },
-  firstName:{
     type: String,
-    uppercase: true, 
-    required: true
+    required: true,
+  },
+  firstName: {
+    type: String,
+    uppercase: true,
+    required: true,
   },
   lastName: {
     type: String,
     uppercase: true,
-    required:true
+    required: true,
   },
-  zipCode:{
-    type: String, 
-    required: true
-  }
-}
-);
+  zip: {
+    type: String,
+    minlength: 5,
+    maxlength: 9,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d/.test(v);
+      },
+      message: (props) => `${props.value} invalid zip`,
+    },
+  },
+});
 
-module.exports = User = mongoose.model('User', UserSchema);
+module.exports = User = mongoose.model("User", UserSchema);

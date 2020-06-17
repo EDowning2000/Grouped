d5cf7c600c734472dc70cefb885792c36c17e289
EDowning2000 <emersondowning@gmail.com>
2020-06-16 21:44:59 -0400

    ready for kyle to pull

---
+++ client/src/components/SingleEvent/SingleEvent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.scss'
+
+
+export default class SingleEvent extends React.Component {
+constructor(props) {
+    super(props)
+    
+}
+render(props) {
+    let event = props.SingleEvent;
+    return(
+        <div class="jumbotron">
+              <h1 class="display-4">{event.name}</h1>
+              <h2>{event.host}</h2>
+              <hr class="my-4"></hr>
+
+              <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+              </p>
+            </div>
+    )
+}
+}
+© 2020 GitH
